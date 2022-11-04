eb268102302d9a70d8691f227e9a7baa1c1ed1d4
Renan Maia <renanmaia.rl@gmail.com>
2022-11-04 11:37:31 -0300

    add quiz

---
+++ app.js
@@ -0,0 +1,117 @@
+
+const correctAnswer = ['C', 'C', 'C', 'C'];
+
+const form = document.querySelector('form');
+const pEn = document.querySelectorAll('#enus');
+const pPt = document.querySelectorAll('#ptbr');
+const labelEn = document.querySelector('.translate-en');
+const labelPt = document.querySelector('.translate-pt');
+const popup = document.querySelector('.popup-wrapper');
+const popupContent = document.querySelector('.popup-content');
+
+
+
+labelEn.addEventListener('click', () => {
+
+    //maybe function
+     pEn.forEach(p => {
+        p.classList.remove('hidden');
+    });
+
+    pPt.forEach(p => {
+        p.classList.add('hidden');
+    });
+    
+});
+
+
+labelPt.addEventListener('click', () => {
+
+    //maybe function
+    pPt.forEach(p => {
+        p.classList.remove('hidden');
+    });
+
+    pEn.forEach(p => {
+        p.classList.add('hidden');
+    });
+
+});
+
+
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+
+    const popupContentParagraphPt = popupContent.childNodes[3];
+    const popupContentParagraphEn = popupContent.childNodes[5];
+
+    
+    let scoreUser = 0;
+
+    const answerUser = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value
+    ];
+
+    answerUser.forEach((answer, index) => {
+        
+        if (answer === correctAnswer[index]) {
+            scoreUser += 25;
+        }
+
+    });
+
+    //vai virar function kk 
+    if (scoreUser === 0) {
+
+       popup.classList.remove('hidden');
+       popupContentParagraphPt.textContent = `Calma jovem, tenta de novo <3! Você não acertou nenhuma alternativa, mas não significa nada! =)`
+       popupContentParagraphEn.textContent = `Your score is 0 :(`;
+
+    } else if (scoreUser === 25) {
+
+        popup.classList.remove('hidden');
+        popupContentParagraphPt.textContent = `Boa, ${scoreUser}% do quiz! mas da pra melhorar!`
+        popupContentParagraphEn.textContent = `Nice, your score is ${scoreUser}% of the quiz!`
+
+    } else if (scoreUser === 50) {
+
+        popup.classList.remove('hidden');
+        popupContentParagraphPt.textContent = `Muito bom, ${scoreUser}% do quiz! Vamo que da pra fechar o quiz!`
+        popupContentParagraphEn.textContent = `Very good, your score is ${scoreUser}% of the quiz!`
+
+    } else if (scoreUser === 75) {
+
+        popup.classList.remove('hidden');
+        popupContentParagraphPt.textContent = `Show, acertou ${scoreUser}%! Quase perfeito hein?! Vamo acertar tudo agora?`
+        popupContentParagraphEn.textContent = `Good job! Your score is ${scoreUser}% of the quiz!`
+
+    } else {
+
+        popup.classList.remove('hidden');
+        popupContentParagraphPt.textContent = `Perfeito! você acertou ${scoreUser}% do quiz`
+        popupContentParagraphEn.textContent = `Perfect! ${scoreUser}% of the quiz`
+
+    };
+
+});
+
+
+
+/** Fechar popup */
+popup.addEventListener('click', event => {
+
+    const getClassClicked = event.target.classList[0];
+
+    if (getClassClicked !== null) {
+        popup.classList.add('hidden');
+    }
+
+    document.location.reload(true);
+    
+})
